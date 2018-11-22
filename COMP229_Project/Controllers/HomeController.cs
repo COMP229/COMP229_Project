a8fc56de6e67b6837363a062bdd0c696ad3945c3
Logan Junhwi Kim <35975380+logankim0212@users.noreply.github.com>
2018-11-21 19:08:45 -0500

    update

--- COMP229_Project/Controllers/HomeController.cs
+++ COMP229_Project/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace COMP229_Project.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page. HELLO!!!";
 
             return View();
         }
