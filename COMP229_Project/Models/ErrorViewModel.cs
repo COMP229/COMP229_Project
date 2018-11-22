ce5e9352af57822cae90ec490507b2a21e395f01
Logan Junhwi Kim <35975380+logankim0212@users.noreply.github.com>
2018-11-21 19:00:21 -0500

    Add project files.

---
+++ COMP229_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace COMP229_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
