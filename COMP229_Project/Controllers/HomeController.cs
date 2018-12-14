3299f29d0ebb00654e826eff7bd902a199f6735d
arshaujla <45246824+arshaujla@users.noreply.github.com>
2018-12-14 00:12:22 -0500

    update

--- COMP229_Project/Controllers/HomeController.cs
+++ COMP229_Project/Controllers/HomeController.cs
@@ -29,6 +29,12 @@ namespace COMP229_Project.Controllers
             return View();
         }
 
+        public IActionResult myWatch()
+        {
+            ViewData["Message"] = "How about myWatch???";
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
